f58e7b3f85fb4639cc13b678241916c31aaee3df
javiermayan <44719141+javiermayan@users.noreply.github.com>
2020-02-09 02:07:13 -0300

    Create js-Passing-Values-to-Functions-with-Arguments.js

---
+++ js-Passing-Values-to-Functions-with-Arguments.js
@@ -0,0 +1,4 @@
+function functionWithArgs(a, b) { //declaramos la función
+  console.log (a + b);  //tarea a realizar: suma de sus parámetros
+} 
+functionWithArgs(5, 8); //llamamos a la función y le pasamos el valor de las variables en sus argumentos
